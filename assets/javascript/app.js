4632e7a67fa4afd256fe69069729cc7eacec6cf6
Roy Norton <rnorton12@centurylink.net>
2017-12-03 19:33:07 -0700

    making progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,57 @@ $(document).ready(function () {
 
     var animalTypeArray = ["dog", "cat", "rabbit", "chicken", "bird", "sugar glider", "frog"];
     var animalObjectArray = [];
-    var GIFSTATE = ["still", "animated"];
-    var API_KEY = "X1V2BUS0MZ6grwEYwEVTno1za1ICpU2N";
-    var currentSearchTerm = "";
-    var LIMIT = 10;
-    var increment = 0;
-    
+
+    var gifApiObject = {
+        gifState: {
+            still: "still",
+            animated: "animated"
+        },
+        apiKey: "X1V2BUS0MZ6grwEYwEVTno1za1ICpU2N",
+        searchTerm: "",
+        resultsLimit: 10,
+        resultsOffset: 0
+    };
+
+    var headingCharacters = ["G", "I", "F", "T", "A", "S", "T", "I", "C"];
+    headingObjectArray = [];
+
+    // animated letters for the page heading
+    // will spell GIFTASTIC
+    gifLetterImageObject = {
+        letterA: "./assets/images/animated-letter-image-A.gif",
+        letterC: "./assets/images/animated-letter-image-C.gif",
+        letterF: "./assets/images/animated-letter-image-F.gif",
+        letterG: "./assets/images/animated-letter-image-G.gif",
+        letterI: "./assets/images/animated-letter-image-I.gif",
+        letterS: "./assets/images/animated-letter-image-S.gif",
+        letterT: "./assets/images/animated-letter-image-T.gif"
+    };
+
+    function hideBackButton() {
+        $("#back-button").hide();
+    }
+
+    function showBackButton() {
+        $("#back-button").show();
+    }
+
+    function disableBackButton() {
+        $("#back-button").prop("disabled", true);
+    }
+
+    function enableBackButton() {
+        $("#back-button").prop("disabled", false);
+    }
+
+    function hideNextButton() {
+        $("#next-button").hide();
+    }
+
+    function showNextButton() {
+        $("#next-button").show();
+    }
+
     function removeButtons() {
         $(".animal").remove();
     }
@@ -16,8 +61,52 @@ $(document).ready(function () {
         // remove any previous gifs displayed
         $(".button-image").remove();
     }
-    function queryGifs(searchTerm, api_key, offset) {
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + api_key + "&limit=10&offset=" + offset;
+
+    function createHeading() {
+        for (var i = 0; i < headingCharacters.length; i++) {
+            var letterObject = {
+                letter: headingCharacters[i],
+                image: "",
+                width: "50px",
+                height: "50px"
+            };
+
+            if (headingCharacters[i].toUpperCase() === "A") {
+                letterObject.image = gifLetterImageObject.letterA;
+            } else if (headingCharacters[i].toUpperCase() === "C") {
+                letterObject.image = gifLetterImageObject.letterC;
+            } else if (headingCharacters[i].toUpperCase() === "F") {
+                letterObject.image = gifLetterImageObject.letterF;
+            } else if (headingCharacters[i].toUpperCase() === "G") {
+                letterObject.image = gifLetterImageObject.letterG;
+            } else if (headingCharacters[i].toUpperCase() === "I") {
+                letterObject.image = gifLetterImageObject.letterI;
+            } else if (headingCharacters[i].toUpperCase() === "S") {
+                letterObject.image = gifLetterImageObject.letterS;
+            } else {
+                letterObject.image = gifLetterImageObject.letterT;
+            }
+
+            headingObjectArray.push(letterObject);
+        }
+    }
+
+    function displayHeading() {
+        createHeading();
+        for (var i = 0; i < headingObjectArray.length; i++) {
+            var $imageTag = $("<img>");
+            $imageTag.attr("src", headingObjectArray[i].image);
+            $imageTag.attr("alt", headingObjectArray[i].letter);
+            $imageTag.attr("width", headingObjectArray[i].width);
+            $imageTag.attr("height", headingObjectArray[i].height);
+
+            $("#heading").append($imageTag);
+        }
+    }
+
+    function queryGifs(searchTerm, api_key, limit, offset) {
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + api_key + "&limit=" + limit + "&offset=" + offset;
+        console.log(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -27,31 +116,31 @@ $(document).ready(function () {
             // reset the object array
             animalObjectArray = [];
 
-            console.log(response);
+            console.log(response.data.length);
             for (var i = 0; i < response.data.length; i++) {
                 var gifObject = {
                     type: "",
                     picture: "",
                     gif: "",
-                    title: "",
                     rating: "",
                     state: "" // still
                 };
                 gifObject.picture = response.data[i].images.fixed_width_still.url;
                 gifObject.gif = response.data[i].images.fixed_width_small.url;
-                gifObject.title = response.data[i].title;
                 gifObject.rating = response.data[i].rating;
-                gifObject.state = GIFSTATE[0];
+                gifObject.state = gifApiObject.gifState.still;
 
                 animalObjectArray.push(gifObject);
                 displayGifs(i);
+                offset++;
             } // end for loop
-            increment++;
+            console.log(offset);
         });
     }
 
     function renderButtons() {
-        //removeGifs();
+        hideBackButton();
+        hideNextButton();
 
         for (var i = 0; i < animalTypeArray.length; i++) {
             // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
@@ -90,7 +179,7 @@ $(document).ready(function () {
         // the bottom caption will contain the characters starting health points
         var $figCaptionBottom = $("<figcaption>");
         $figCaptionBottom.addClass("figure-caption");
-//        $figCaptionBottom.text(animalObjectArray[index].title);
+        //        $figCaptionBottom.text(animalObjectArray[index].title);
         $figCaptionBottom.text("");
 
         // Add an image to the button element
@@ -114,48 +203,79 @@ $(document).ready(function () {
 
     function animateGif(id, index) {
         console.log("animateGif");
-        
+
         var imageSrc = "";
 
-        if (animalObjectArray[index].state === GIFSTATE[0]) { // still
+        if (animalObjectArray[index].state === gifApiObject.gifState.still) { // still
             // switch to animated image
             imageSrc = animalObjectArray[index].gif;
 
             // update the state
-            animalObjectArray[index].state = GIFSTATE[1];
+            animalObjectArray[index].state = gifApiObject.gifState.animated;
 
         } else { // animated
             // switch to still image
             imageSrc = animalObjectArray[index].picture;
 
             // update the state
-            animalObjectArray[index].state = GIFSTATE[0];
+            animalObjectArray[index].state = gifApiObject.gifState.still;
         }
         // update the image src attribute
         $("#" + id).attr("src", imageSrc);
     }
 
-    function getMoreGifs(searchTerm) {
-        removeGifs();
-        
-            offset = increment * LIMIT;
-            increment++;
-        
-        
-        queryGifs(searchTerm, API_KEY, offset);
+    function getMoreGifsBack(searchTerm) {
+        var searchTerm = gifApiObject.searchTerm;
+        var apiKey = gifApiObject.apiKey;
+        var limit = gifApiObject.resultsLimit;
+        var offset = gifApiObject.resultsOffset;
 
+        offset -= limit;
+        if (offset <= 0) {
+            offset = 0;
+            disableBackButton();
+        }
+        gifApiObject.resultsOffset = offset;
+
+        console.log("back-offset: " + offset);
+        removeGifs();
+        queryGifs(searchTerm, apiKey, limit, offset);
     }
 
+    function getMoreGifsNext(searchTerm) {
+        var searchTerm = gifApiObject.searchTerm;
+        var apiKey = gifApiObject.apiKey;
+        var limit = gifApiObject.resultsLimit;
+        var offset = gifApiObject.resultsOffset;
+
+        offset += limit;
+        gifApiObject.resultsOffset = offset;
+
+        console.log("next-offset: " + offset);
+        removeGifs();
+        queryGifs(searchTerm, apiKey, limit, offset);
+        enableBackButton();
+    }
+
+    displayHeading();
     renderButtons();
 
     // Adding a click event listener to all elements with a class of "movie"
     $(document).on("click", ".animal", function () {
-        currentSearchTerm = $(this).attr("data-name");
-        offset = 0;
-        increment = 0;
-        removeGifs();
+        var searchTerm = $(this).attr("data-name");
+        var apiKey = gifApiObject.apiKey;
+        var limit = gifApiObject.resultsLimit;
+        var offset = 0;
 
-        queryGifs(currentSearchTerm, API_KEY, offset);
+        gifApiObject.searchTerm = searchTerm;
+        gifApiObject.resultsOffset = offset;
+
+        console.log("animal-offset: " + offset);
+        showBackButton();
+        disableBackButton();
+        showNextButton();
+        removeGifs();
+        queryGifs(searchTerm, apiKey, limit, offset);
     });
 
     $(document).on("click", ".myImage", function () {
@@ -167,15 +287,23 @@ $(document).ready(function () {
         animateGif(gifId, gifValue);
     });
 
-    $(document).on("click", ".moreGifs", function () {
-        getMoreGifs(currentSearchTerm);
+    $(document).on("click", "#back-button", function () {
+        var searchTerm = gifApiObject.searchTerm;
+        getMoreGifsBack(searchTerm);
     });
 
-    $("#user-input").on("click", function () {
+    $(document).on("click", "#next-button", function () {
+        var searchTerm = gifApiObject.searchTerm;
+        getMoreGifsNext(searchTerm);
+    });
+
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        var newItem = $("#new-item").val().trim();
+        console.log(newItem);
         removeGifs();
         removeButtons();
-        console.log($(this).val());
-        animalTypeArray.push($(this).val());
+        animalTypeArray.push(newItem);
         renderButtons();
     });
 
